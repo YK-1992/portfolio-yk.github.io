a79956bd3280f114ee811d0d4c5d0a6c7984fc0f
YK-1992 <179270256+YK-1992@users.noreply.github.com>
2024-10-27 19:31:54 +0100

    new file

--- portfolio-YK/src/components/Projects/Projects.jsx
+++ portfolio-YK/src/components/Projects/Projects.jsx
@@ -1,165 +1,187 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
-import { forwardRef } from 'react';
-import './projects.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { forwardRef } from "react";
+import "./projects.css";
 
+const Projects = forwardRef((props, ref) => {
+  return (
+    <section ref={ref} className="section_block">
+      <div className="projects-container">
+        <div className="proj-title-section">
+          <h1 className="gradient-text text-fonts">Portfolio</h1>
+          <p>More recent work</p>
+        </div>
 
-const Projects = forwardRef ((props, ref) => {
-        return(
-            <section ref={ref} className="section_block" >
-                <div className="projects-container">
-                    <div className="proj-title-section">
-    <h1 className='gradient-text text-fonts'>Portfolio</h1>
-    <p >More recent work</p>
+        <Swiper pagination={true} modules={[Pagination]}>
+          <SwiperSlide>
+            {/* Project1 */}
+            <div className="project__block project1">
+              <div className="img-title">
+                <img src="./logo-yks-ervice.png" alt="" className="icon-style" />
+                <div className="overlay">
+                  <div className="overlay-tex">
+                    <h2>YK Services</h2>
+                    <div className="create-lang">
+                      <p>React </p>
+                      <p>Scss</p>
                     </div>
-
-                       
-                        <Swiper pagination={true} modules={[Pagination]} >
-
-     <SwiperSlide>
-   {/* Project1 */}
-   <div className="project__block project1">
-                        <div className="img-title">
-                            <img src="./img1.jpg" alt=""  className='icon-style'/>
-                            <div className="overlay">
-                                <div className="overlay-tex">
-                                    <h2>Project 1</h2>
-                                    <div className="create-lang">
-                                        <span>React</span>
-                                        <span>Scss</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project__info-block">
-                            <h3 className='text-st' >Name Project1</h3>
-                            <p className='text-st-p'> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                 Magni eveniet sequi rerum quaerat est similique officia magnam
-                                  hic exercitationem molestiae accusantium quod architecto delectus 
-                                  necessitatibus  illum, recusandae voluptas. Harum, delectus!
-                            </p>
-
-                            <div className="projects-demo">
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Code</span> <img src="./github.png" alt="github" className='logo' />  </a>
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Demo</span> <img src="./link.png" alt="github" className='logo' />  </a>
-                            </div>
-                        </div>   
-                    </div>
-     </SwiperSlide>
-    
-
-    <SwiperSlide>
-          {/* Project2 */}
-                       
-          <div className="project__block project1">
-                        <div className="img-title">
-                            <img src="./img2.jpg" alt=""  className='icon-style'/>
-                            <div className="overlay">
-                                <div className="overlay-tex">
-                                    <h2>Project 1</h2>
-                                    <div className="create-lang">
-                                        <span>React</span>
-                                        <span>Scss</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project__info-block">
-                            <h3 className='text-st' >Name Project1</h3>
-                            <p className='text-st-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                 Magni eveniet sequi rerum quaerat est similique officia magnam
-                                  hic exercitationem molestiae accusantium quod architecto delectus 
-                                  necessitatibus  illum, recusandae voluptas. Harum, delectus!
-                            </p>
-
-                            <div className="projects-demo">
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Code</span> <img src="./github.png" alt="github" className='logo' />  </a>
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Demo</span> <img src="./link.png" alt="github" className='logo' />  </a>
-                            </div>
-                        </div>   
-                    </div>
-    </SwiperSlide>
-   
-    <SwiperSlide>
-  {/* Project3 */}
-
-  <div className="project__block project1">
-                        <div className="img-title">
-                            <img src="./img3.jpg" alt=""  className='icon-style'/>
-                            <div className="overlay">
-                                <div className="overlay-tex">
-                                    <h2>Project 1</h2>
-                                    <div className="create-lang">
-                                        <span>React</span>
-                                        <span>Scss</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project__info-block">
-                            <h3 className='text-st' >Name Project1</h3>
-                            <p className='text-st-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                 Magni eveniet sequi rerum quaerat est similique officia magnam
-                                  hic exercitationem molestiae accusantium quod architecto delectus 
-                                  necessitatibus  illum, recusandae voluptas. Harum, delectus!
-                            </p>
-
-                            <div className="projects-demo">
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Code</span> <img src="./github.png" alt="github" className='logo' />  </a>
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Demo</span> <img src="./link.png" alt="github" className='logo' />  </a>
-                            </div>
-                        </div>   
-                    </div>
-    </SwiperSlide>
-   
-    <SwiperSlide>
- {/* Project4 */}
- <div className="project__block project1">
-                        <div className="img-title">
-                            <img src="./img4.jpg" alt=""  className='icon-style'/>
-                            <div className="overlay">
-                                <div className="overlay-tex">
-                                    <h2>Project 1</h2>
-                                    <div className="create-lang">
-                                        <span>React</span>
-                                        <span>Scss</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="project__info-block">
-                            <h3 className='text-st' >Name Project1</h3>
-                            <p className='text-st-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                 Magni eveniet sequi rerum quaerat est similique officia magnam
-                                  hic exercitationem molestiae accusantium quod architecto delectus 
-                                  necessitatibus  illum, recusandae voluptas. Harum, delectus!
-                            </p>
-
-                            <div className="projects-demo">
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Code</span> <img src="./github.png" alt="github" className='logo' />  </a>
-                            <a href="https://github.com/YK-1992/projectDemo"> <span>Demo</span> <img src="./link.png" alt="github" className='logo' />  </a>
-                            </div>
-                        </div>   
-                    </div>
-    </SwiperSlide>
-                   
-                      
-                       
-
-                        </Swiper>
-                      
-                      
-                  
-
+                  </div>
                 </div>
-            </section>
-        )
+              </div>
+              <div className="project__info-block">
+                <h3 className="text-st">YK Services</h3>
+                <p className="text-st-p">
+                  {" "}
+                  Welcome to YK Services – your go-to solution 
+                  for all home service needs! From professional cleaning
+                  and pet care to small repairs and moving support!
+                </p>
 
-    }); 
+                <div className="projects-demo">
+                  <a href="https://github.com/YK-1992/My-Portfolio/tree/project-Cleaning/Project-Cleaning">
+                    {" "}
+                    <span>Code</span>{" "}
+                    <img src="./github.png" alt="github" className="logo" />{" "}
+                  </a>
+                  <a href="../Project-Cleaning/">
+                    {" "}
+                    <span>Demo</span>{" "}
+                    <img src="./link.png" alt="github" className="logo" />{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
 
+          <SwiperSlide>
+            {/* Project2 */}
 
-Projects.displayName= "Projects"
+            <div className="project__block project1">
+              <div className="img-title">
+                <img src="./img2.jpg" alt="" className="icon-style" />
+                <div className="overlay">
+                  <div className="overlay-tex">
+                    <h2>Project 1</h2>
+                    <div className="create-lang">
+                      <span>React</span>
+                      <span>Scss</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="project__info-block">
+                <h3 className="text-st">Name Project1</h3>
+                <p className="text-st-p">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                  eveniet sequi rerum quaerat est similique officia magnam hic
+                  exercitationem molestiae accusantium quod architecto delectus
+                  necessitatibus illum, recusandae voluptas. Harum, delectus!
+                </p>
+
+                <div className="projects-demo">
+                  <a href="https://github.com/YK-1992/projectDemo">
+                    {" "}
+                    <span>Code</span>{" "}
+                    <img src="./github.png" alt="github" className="logo" />{" "}
+                  </a>
+                  <a href="https://github.com/YK-1992/projectDemo">
+                    {" "}
+                    <span>Demo</span>{" "}
+                    <img src="./link.png" alt="github" className="logo" />{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            {/* Project3 */}
+
+            <div className="project__block project1">
+              <div className="img-title">
+                <img src="./img3.jpg" alt="" className="icon-style" />
+                <div className="overlay">
+                  <div className="overlay-tex">
+                    <h2>Project 1</h2>
+                    <div className="create-lang">
+                      <span>React</span>
+                      <span>Scss</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="project__info-block">
+                <h3 className="text-st">Name Project1</h3>
+                <p className="text-st-p">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                  eveniet sequi rerum quaerat est similique officia magnam hic
+                  exercitationem molestiae accusantium quod architecto delectus
+                  necessitatibus illum, recusandae voluptas. Harum, delectus!
+                </p>
+
+                <div className="projects-demo">
+                  <a href="https://github.com/YK-1992/projectDemo">
+                    {" "}
+                    <span>Code</span>{" "}
+                    <img src="./github.png" alt="github" className="logo" />{" "}
+                  </a>
+                  <a href="https://github.com/YK-1992/projectDemo">
+                    {" "}
+                    <span>Demo</span>{" "}
+                    <img src="./link.png" alt="github" className="logo" />{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            {/* Project4 */}
+            <div className="project__block project1">
+              <div className="img-title">
+                <img src="./img4.jpg" alt="" className="icon-style" />
+                <div className="overlay">
+                  <div className="overlay-tex">
+                    <h2>Project 1</h2>
+                    <div className="create-lang">
+                      <span>React</span>
+                      <span>Scss</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="project__info-block">
+                <h3 className="text-st">Name Project1</h3>
+                <p className="text-st-p">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                  eveniet sequi rerum quaerat est similique officia magnam hic
+                  exercitationem molestiae accusantium quod architecto delectus
+                  necessitatibus illum, recusandae voluptas. Harum, delectus!
+                </p>
+
+                <div className="projects-demo">
+                  <a href="https://github.com/YK-1992/projectDemo">
+                    {" "}
+                    <span>Code</span>{" "}
+                    <img src="./github.png" alt="github" className="logo" />{" "}
+                  </a>
+                  <a href="https://github.com/YK-1992/projectDemo">
+                    {" "}
+                    <span>Demo</span>{" "}
+                    <img src="./link.png" alt="github" className="logo" />{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+});
+
+Projects.displayName = "Projects";
 
 export default Projects;
